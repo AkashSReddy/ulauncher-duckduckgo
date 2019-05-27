66a73ff07d758ace8a214916ea66bd2fa975a5da
AkashSReddy <akashsreddy98@gmail.com>
2019-05-27 11:43:53 +0530

    Some changes to main file

--- main.py
+++ main.py
@@ -16,3 +16,36 @@ LOGGER = logging.getLogger(__name__)
 
 def urlencode(qp):
     return urllib.urlencode(qp)
+
+
+class DUCKExtension(Extension):
+
+    def __init__(self):
+        super(DUCKExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+
+
+class KeywordQueryEventListener(EventListener):
+
+    def on_event(self, event, extension):
+        items = list()
+
+        if event.get_argument():
+            LOGGER.info('Showing DuckDuckGo search for "{}"'.format(
+                event.get_argument()))
+            items.append(ExtensionResultItem(
+                icon='images/icon.png',
+                name='Search on Duckduckgo',
+                description='Search for "{}".'.format(event.get_argument()),
+                on_enter=OpenUrlAction(
+                    'https://duckduckgo.com/?q={}'.format(
+                        urlencode({'q': event.get_argument(), 's': 'all'})
+                    ))
+            )
+            )
+
+        return RenderResultListAction(items)
+
+
+if __name__ == '__main__':
+    DUCKExtension().run()
